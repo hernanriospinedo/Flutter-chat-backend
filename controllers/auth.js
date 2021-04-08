be0f6f0bf3e09e1711d1a69a219e5416ed51f059
JOSE HERNAN RIOS PINEDO <hernanriospinedo@gmail.com>
2021-04-08 00:59:25 -0500

    Fin seccion 8

---
+++ controllers/auth.js
@@ -0,0 +1,105 @@
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+
+//const { validationResult } = require("express-validator");
+const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/jwt");
+const usuario = require("../models/usuario");
+
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    const existeEmail = await Usuario.findOne({ email });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El correo ya esta registrado",
+      });
+    }
+
+    //instancia de modelo
+    const usuario = new Usuario(req.body);
+    //encriptar contrasena
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+    await usuario.save();
+
+    //Generar mi JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+  try {
+    const usuarioDB = await Usuario.findOne({ email });
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Email no encontrado",
+      });
+    }
+
+    // Validar el password
+    const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "La contraseña no es valida",
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT(usuarioDB.id);
+
+    res.json({
+      ok: true,
+      usuario: usuarioDB,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return (
+      res.status(500),
+      json({
+        ok: false,
+        msg: "Hable con el administrador",
+      })
+    );
+  }
+};
+
+const renewToken = async (req, res = response) => {
+  const uid = req.uid;
+
+  // generar un nuevo JWT, generarJWT... uid...
+  const token = await generarJWT(uid);
+
+  // Obtener el usuario por el UID, Usuario.findById...
+  const usuario = await Usuario.findById(uid);
+
+  res.json({
+    ok: true,
+    usuario,
+    token,
+  });
+};
+
+module.exports = {
+  crearUsuario,
+  login,
+  renewToken,
+};
---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+/*
+path : api/login
+
+*/
+const { Router } = require("express");
+const { check } = require("express-validator");
+
+const { crearUsuario, login, renewToken } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+
+const router = Router();
+
+router.post(
+  "/new",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "La contraseña  es obligatorio").not().isEmpty(),
+    check("email", "El correo es obligatorio").isEmail(),
+
+    validarCampos,
+  ],
+  crearUsuario
+);
+
+router.post(
+  "/",
+  [
+    check("password", "La contraseña  es obligatorio").not().isEmpty(),
+    check("email", "El correo es obligatorio").isEmail(),
+  ],
+  login
+);
+
+//validarJWT,
+router.get("/renew", validarJWT, renewToken);
+//post /
+//validar campos
+
+module.exports = router;
